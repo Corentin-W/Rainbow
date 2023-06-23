cc65bdc9e309b6054233aa471d68f2504c5ed04b
Corentin-W <corentin.waucampt@gmail.com>
2023-06-23 22:23:27 +0200

    v1-corentin google places ok

--- lib/controllers/login.dart
+++ lib/controllers/login.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../globals/globals.dart';
 import '../services/auth_service.dart';
-import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+// import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -58,14 +58,14 @@ class _LoginState extends State<Login> {
   }
 
   Future<UserCredential?> signInWithFacebook() async {
-    final LoginResult result = await FacebookAuth.instance.login();
-    if (result.status == LoginStatus.success) {
-      // Create a credential from the access token
-      final OAuthCredential credential =
-          FacebookAuthProvider.credential(result.accessToken!.token);
-      // Once signed in, return the UserCredential
-      return await FirebaseAuth.instance.signInWithCredential(credential);
-    }
+    // final  result = await FacebookAuth.instance.login();
+    // if (result.status == LoginStatus.success) {
+    //   // Create a credential from the access token
+    //   final OAuthCredential credential =
+    //       FacebookAuthProvider.credential(result.accessToken!.token);
+    //   // Once signed in, return the UserCredential
+    //   return await FirebaseAuth.instance.signInWithCredential(credential);
+    // }
     return null;
   }
 
